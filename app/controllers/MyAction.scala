dbb07a7e822989f9d73e009fcf0a8d92f6508172
John Lynn <john@eero.com>
2016-01-21 00:07:46 -0800

    Set MDC value in custom action

--- app/controllers/MyAction.scala
+++ app/controllers/MyAction.scala
@@ -1,11 +1,14 @@
 package controllers
 
+import org.slf4j.MDC
 import play.api.mvc.{Result, Action, Request, ActionBuilder}
 import scala.concurrent.{Future, ExecutionContext}
+import scala.util.Random
 
 
 object MyAction extends ActionBuilder[Request] {
   def invokeBlock[A](request: Request[A], block: (Request[A]) => Future[Result]) = {
+    MDC.put("user_id", Random.nextInt(10).toString)
     block(request)
   }
   override def executionContext: ExecutionContext = concurrent.Execution.defaultContext
